309926ec25def59e6e5a2a95f3522800c07cc460
Tiago Edgar Rasia <103585675+rasia83@users.noreply.github.com>
2022-09-25 22:47:17 -0700

    Add files via upload

---
+++ iac1.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# nao esquecer
+# chmod +x iac1.sh
+
+# Excluir diretórios, arquivos, grupos e usuários criados anteriormente;
+# Todo provisionamento deve ser feito em um arquivo do tipo Bash Script;
+# O dono de todos os diretórios criados será o usuário root;
+# Todos os usuários terão permissão total dentro do diretório publico;
+# Os usuários de cada grupo terão permissão total dentro de seu respectivo diretório;
+# Os usuários não poderão ter permissão de leitura, escrita e execução em diretórios de departamentos que eles não pertencem;
+# Subir arquivo de script criado para a sua conta no GitHub.
+
+
+echo "Criando diretórios..."
+
+mkdir /publico
+mkdir /adm
+mkdir /ven
+mkdir /sec
+
+echo "Criando grupos de usuários..."
+
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+echo "Criando usuários..."
+
+useradd carlos -m -s /bin/bash -p $(echo Senha123 | openssl passwd --stdin) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(echo Senha123 | openssl passwd --stdin) -G GRP_ADM
+useradd joao -m -s /bin/bash -p $(echo Senha123 | openssl passwd --stdin) -G GRP_ADM
+
+useradd debora -m -s /bin/bash -p $(echo Senha123 | openssl passwd --stdin) -G GRP_VEN
+useradd sebastiana -m -s /bin/bash -p $(echo Senha123 | openssl passwd --stdin) -G GRP_VEN
+useradd roberto -m -s /bin/bash -p $(echo Senha123 | openssl passwd --stdin) -G GRP_VEN
+
+useradd josefina -m -s /bin/bash -p $(echo Senha123 | openssl passwd --stdin) -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $(echo Senha123 | openssl passwd --stdin) -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $(echo Senha123 | openssl passwd --stdin) -G GRP_SEC
+
+echo "Especificando permissões dos diretórios...."
+
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+chmod 777 /publico
+
+echo "Fim....."
+
+
+
+
+
+
+
+
